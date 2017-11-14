d98ba202558e4fb0b37cb178d227d3c44a2dc330
Rishi Banerjee <rishieric91@gmail.com>
2017-11-15 01:54:03 +0530

    Existing CSS File updated, Login Page Modified

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,78 +1,126 @@
-/*
-This is the LoginPage class which creates the LoginPage for the user.
- */
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class LoginPage extends Application
-{
-    Stage window;
-    public static void main(String args[])
-    {
+/**
+ *
+ * @web http://zoranpavlovic.blogspot.com/
+ */
+public class LoginPage extends Application {
+
+    String user = "JavaFX2";
+    String pw = "password";
+    String checkUser, checkPw;
+
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage)throws Exception
-    {
-        window = primaryStage;
-        window.setTitle("Mandreck 1.0");
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Mandy");
 
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(10);
-        grid.setHgap(10);
+        BorderPane bp = new BorderPane();
+        bp.setPadding(new Insets(10,50,50,50));
 
-        Label nameLabel = new Label("Username : ");
-        nameLabel.setId("bold-label");
-       // nameLabel.setStyle("-fx-text-fill: #eBeBeB");
-        GridPane.setConstraints(nameLabel,0,0);
+        //Adding HBox
+        HBox hb = new HBox();
+        hb.setPadding(new Insets(20,20,20,30));
 
-        TextField nameInput = new TextField();
-        nameInput.setPromptText("Username");
-        GridPane.setConstraints(nameInput,1,0);
+        //Adding GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20,20,20,20));
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
 
-        Label passLabel = new Label("Password : ");
-        //passLabel.setStyle("-fx-text-fill: #eBeBeB");
-        passLabel.setId("bold-label");
-        GridPane.setConstraints(passLabel,0,1);
+        //Implementing Nodes for GridPane
+        Label lblUserName = new Label("Username");
+        final TextField txtUserName = new TextField();
+        Label lblPassword = new Label("Password");
+        final PasswordField pf = new PasswordField();
+        Button btnLogin = new Button("Login");
+        final Label lblMessage = new Label();
 
-        TextField passInput = new TextField();
-        passInput.setPromptText("**********");
-        GridPane.setConstraints(passInput,1,1);
+        //Adding Nodes to GridPane layout
+        gridPane.add(lblUserName, 0, 0);
+        gridPane.add(txtUserName, 1, 0);
+        gridPane.add(lblPassword, 0, 1);
+        gridPane.add(pf, 1, 1);
+        gridPane.add(btnLogin, 2, 1);
+        gridPane.add(lblMessage, 1, 2);
 
-        Button loginButton = new Button("Login");
-        GridPane.setConstraints(loginButton,1,2);
 
-        Button signUp = new Button("Sign Up!");
-        GridPane.setConstraints(signUp,1,3);
+        //Reflection for gridPane
+        Reflection r = new Reflection();
+        r.setFraction(0.7f);
+        gridPane.setEffect(r);
 
-        signUp.setOnAction( e-> {
-            try {
-                StoreLoginInfo info = new StoreLoginInfo();
-                String username = nameInput.getText();
-                String password = passInput.getText();
-                info.storeLoginInfo("\n"+username+"\n");
-                info.storeLoginInfo("\n"+password+"\n");
-            }
-            catch (Exception exp)
-            {}
+        //DropShadow effect
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setOffsetX(5);
+        dropShadow.setOffsetY(5);
+
+        //Adding text and DropShadow effect to it
+        Text text = new Text("Mandy");
+        text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
+        text.setEffect(dropShadow);
+
+        //Adding text to HBox
+        hb.getChildren().add(text);
+
+        //Add ID's to Nodes
+        bp.setId("bp");
+        gridPane.setId("root");
+        btnLogin.setId("btnLogin");
+        text.setId("text");
+
+        //Action for btnLogin
+        btnLogin.setOnAction(e -> {
+
+                checkUser = txtUserName.getText().toString();
+                checkPw = pf.getText().toString();
+                if(checkUser.equals(user) && checkPw.equals(pw)){
+                    lblMessage.setText("Congratulations!");
+                    lblMessage.setTextFill(Color.GREEN);
+                }
+                else{
+                    lblMessage.setText("Incorrect user or pw.");
+                    lblMessage.setTextFill(Color.RED);
+                }
+                txtUserName.setText("");
+                pf.setText("");
+
         });
 
-        grid.getChildren().addAll(nameLabel,nameInput,passLabel,passInput,loginButton,signUp);
-        Scene scene = new Scene(grid,300,200);
-        scene.getStylesheets().add("MandreckStyleSheet.css");
+        //Add HBox and GridPane layout to BorderPane Layout
+        bp.setTop(hb);
+        bp.setCenter(gridPane);
 
-
-        window.setScene(scene);
-        window.show();
-
-
+        //Adding BorderPane to the scene and loading CSS
+        Scene scene = new Scene(bp);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("login.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.titleProperty().bind(
+                scene.widthProperty().asString().
+                        concat(" : ").
+                        concat(scene.heightProperty().asString()));
+        //primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
